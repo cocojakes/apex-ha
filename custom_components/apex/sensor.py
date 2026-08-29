628e6a20f66c41f6740aaa354d8d52c514fbce66
bugbountyguy <88092414+bugbountyguy@users.noreply.github.com>
2026-08-28 21:01:37 -0500

    Add better handling of advanced sensor types with tdata, and include mxm light data

--- custom_components/apex/sensor.py
+++ custom_components/apex/sensor.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import logging
 import re
 
@@ -10,6 +12,9 @@ from . import ApexEntity
 from .const import DOMAIN, SENSORS, MEASUREMENTS, MANUAL_SENSORS
 
 _LOGGER = logging.getLogger(__name__)
+
+ADVANCED_SENSOR_TYPES = ["MXMLight|Ecotech|15G6P", "virtual", "variable", "cor|20"]
+
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
 
@@ -105,7 +110,7 @@ class ApexSensor(ApexEntity, SensorEntity):
                         return value["status"][1]
                     if self.sensor["type"] == "vortech":
                         return f"{value["status"][0]} {value["status"][1]} {value["status"][2]}"
-                    if self.sensor["type"] == "virtual" or self.sensor["type"] == "variable" or self.sensor["type"] == "cor|20":
+                    if self.sensor["type"] in ADVANCED_SENSOR_TYPES:
                         if "config" in self.coordinator.data:
                             config_data = self.coordinator.data["config"]
                             if "oconf" in config_data:
@@ -146,14 +151,66 @@ class ApexSensor(ApexEntity, SensorEntity):
     def process_prog(self, prog):
         if len(prog) > 255:
             return None
+        
         if "Set PF" in prog:
             return prog
+        
         test = re.findall("Set\s[^\d]*(\d+)", prog)
         if test:
             _LOGGER.debug(test[0])
             return int(test[0])
-        else:
-            return prog     
+        
+        test = re.findall("tdata\s[\d,:]*", prog)
+        if test:
+            # tdata is basically set over time, so first we need to get the current time and compare it to the two closest
+            # points available. If there's only 1 point, we know it's a constant value.
+            if len(test) == 1:
+                # Example with a constant pump: "prog": "Fallback OFF \ntdata 18:29:00,0,0,75,0,0,0,0,0,0,0,0,0,0\n",
+                values = test[0].split(',')
+                return values[3]
+            
+            # More than 1 point, now to determine the time and value
+            current_time = datetime.now().time()
+            curr_frame = None
+            next_frame = None
+            
+            for line in test:
+                split_line = line.split(',')
+                line_time = datetime.strptime(split_line[0], "tdata %H:%M:%S").time()
+                
+                if current_time >= line_time:
+                    curr_frame = split_line
+                elif next_frame is None:
+                    next_frame = split_line
+                    break
+            
+            # If we hit one of these scenarios, it means we're outside the time range, which means the value
+            # is going to simply be whatever it is in the first or final frame
+            if curr_frame is None:
+                return int(test[0].split(',')[3])
+            elif next_frame is None:
+                return int(test[-1].split(',')[3])
+
+            # Okay, we're within the time ranges... Now get the times and intensities in between the two points
+            curr_frame_time = datetime.strptime(curr_frame[0], "tdata %H:%M:%S").time()
+            next_frame_time = datetime.strptime(next_frame[0], "tdata %H:%M:%S").time()
+            curr_frame_num = int(curr_frame[3])
+            next_frame_num = int(next_frame[3])
+
+            # Grab the difference in time and get the % of its location
+            curr_frame_sec = curr_frame_time.hour * 3600 + curr_frame_time.minute * 60 + curr_frame_time.second
+            next_frame_sec = next_frame_time.hour * 3600 + next_frame_time.minute * 60 + next_frame_time.second
+            time_now_sec = current_time.hour * 3600 + current_time.minute * 60 + current_time.second
+            # Example:
+            # 11:00:00 - 13:30:00, time now is 12:00:00. Values 60 and 75 respectively.
+            # 2.5 hours in between, but we're only 1 hour past 11, so 1/2.5 indicates we're 40% the way there between 60->75, or (75-60)*0.4 = 6.0 => 60+6 = 66% intensity
+            curr_time_pos = time_now_sec - curr_frame_sec
+            total_time_diff = next_frame_sec - curr_frame_sec
+            percent_distance = curr_time_pos / total_time_diff
+            offset = (next_frame_num - curr_frame_num) * percent_distance
+            return curr_frame_num + offset
+        
+        return prog
     
     @property
     def name(self):
